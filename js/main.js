ba7b240b4d0152aef939e8393a6608cb6ab5d1e7
Matthew Korporaal <matthew.korporaal@gmail.com>
2013-12-22 23:41:58 -0800

    fixed folders

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
-$(document).ready(function(){
-  $('#loader').hide();
-  $("#git-repos").on("click", function(){
-    $.ajax({
-      beforeSend: function(e){
-        e.preventDefault();
-        $('#loader').show();
-      },
-      success: function(data){
-      console.log(this);
-        setTimeout(function(){
-          $('#loader').hide();
-          $('.wrapper').empty();
-          $('.wrapper').html(data['Title']);
-        }, 100000);
-      }
-    });
-  });
-});
+// $(document).ready(function(){
+//   $('#loader').hide();
+//   $("#git-repos").on("click", function(){
+//     $.ajax({
+//       beforeSend: function(e){
+//         e.preventDefault();
+//         $('#loader').show();
+//       },
+//       success: function(data){
+//       console.log(this);
+//         setTimeout(function(){
+//           $('#loader').hide();
+//           $('.wrapper').empty();
+//           $('.wrapper').html(data['Title']);
+//         }, 100000);
+//       }
+//     });
+//   });
+// });
 
-(function(){
-  var delay = 3000;
-  window.setTimeout(function(){$('#downloadLink')[0].click();},delay);
-})();
+// (function(){
+//   var delay = 3000;
+//   window.setTimeout(function(){$('#downloadLink')[0].click();},delay);
+// })();
